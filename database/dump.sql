e5b13f8046ee0dc08cd6ae72f3895a7f70302726
jettblu <jettblu.fabrications@gmail.com>
2026-02-17 12:41:40 -0800

    Update dump with view helper

--- database/dump.sql
+++ database/dump.sql
@@ -990,6 +990,28 @@ $$;
 ALTER FUNCTION "private"."has_good_view_role"("user_id_caller" "uuid", "herd_id_caller" bigint) OWNER TO "postgres";
 
 
+CREATE OR REPLACE FUNCTION "private"."has_view_role_any_herd"("user_id_caller" "uuid") RETURNS boolean
+    LANGUAGE "plpgsql" SECURITY DEFINER
+    SET "search_path" TO 'extensions'
+    AS $$
+begin
+  return exists (
+    select 1
+    from public.users_roles_per_herd
+    where user_id = user_id_caller
+    and role in ('admin', 'editor', 'viewer')
+  );
+end;
+$$;
+
+
+ALTER FUNCTION "private"."has_view_role_any_herd"("user_id_caller" "uuid") OWNER TO "postgres";
+
+
+COMMENT ON FUNCTION "private"."has_view_role_any_herd"("user_id_caller" "uuid") IS 'Returns true if the user has view role (admin, editor, viewer) in any herd, used for policies that require authenticated users who are part of a herd';
+
+
+
 CREATE OR REPLACE FUNCTION "private"."herd_has_device"("herd_id_caller" bigint, "device_id_caller" bigint) RETURNS boolean
     LANGUAGE "plpgsql" SECURITY DEFINER
     AS $$begin
